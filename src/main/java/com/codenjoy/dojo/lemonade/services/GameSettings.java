5b4876a7cdafbf5607cc832d74a2f32c84fee678
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-02-13 11:12:22 +0200

    [lemonade] The description of the settings is also loaded from the properties file.

--- src/main/java/com/codenjoy/dojo/lemonade/services/GameSettings.java
+++ src/main/java/com/codenjoy/dojo/lemonade/services/GameSettings.java
@@ -24,27 +24,29 @@ package com.codenjoy.dojo.lemonade.services;
 
 
 import com.codenjoy.dojo.services.event.Calculator;
+import com.codenjoy.dojo.services.settings.PropertiesKey;
 import com.codenjoy.dojo.services.settings.SettingsImpl;
 import com.codenjoy.dojo.services.settings.SettingsReader;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static com.codenjoy.dojo.lemonade.services.GameRunner.GAME_NAME;
 import static com.codenjoy.dojo.lemonade.services.GameSettings.Keys.*;
 
 public class GameSettings extends SettingsImpl implements SettingsReader<GameSettings> {
 
-    public enum Keys implements Key {
+    public enum Keys implements PropertiesKey {
 
-        WIN_SCORE("[Score] Win score"),
-        LOSE_PENALTY("[Score] Lose penalty"),
-        BANKRUPT_PENALTY("[Score] Bankrupt penalty"),
-        LIMIT_DAYS("[Game] Limit days");
+        WIN_SCORE,
+        LOSE_PENALTY,
+        BANKRUPT_PENALTY,
+        LIMIT_DAYS;
 
         private String key;
 
-        Keys(String key) {
-            this.key = key;
+        Keys() {
+            this.key = key(GAME_NAME);
         }
 
         @Override
